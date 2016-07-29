5ea2e6f794926b26d6751e9025b94cdfc2ff4351
kdunayczan <katie.dunayczan@gmail.com>
2016-07-29 18:58:48 -0400

    before buck changes

--- board.rb
+++ board.rb
@@ -38,154 +38,154 @@ class Board
     (row >=0 && row <= @row_count-1) && (col >=0 && col <= @col_count-1)
   end
 
-  # def board_full?
-  #   @board.marker.none? { |row| row.include?(" ") }
-  # end
+  def board_full?
+    @board.marker.none? { |row| row.include?(" ") }
+  end
 
-  # def four_consecutive?(angle)
-  #   marker_counter = 1
-  #   @index_counter = 1
-  #   while marker_counter < 4
-  #     if angle
-  #       marker_counter += 1
-  #       if @index_counter < 0
-  #         @index_counter -= 1
-  #       else
-  #         @index_counter += 1
-  #       end
-  #     elsif @index_counter < 0
-  #       break
-  #     else
-  #       @index_counter = -1
-  #     end
-  #   end
-  #   return marker_counter == 4
-  # end
+  def four_consecutive?(angle)
+    marker_counter = 1
+    @index_counter = 1
+    while marker_counter < 4
+      if angle
+        marker_counter += 1
+        if @index_counter < 0
+          @index_counter -= 1
+        else
+          @index_counter += 1
+        end
+      elsif @index_counter < 0
+        break
+      else
+        @index_counter = -1
+      end
+    end
+    return marker_counter == 4
+  end
 
-  # def horizontal?(coordinates)
-  #   row = coordinates[0]
-  #   col = coordinates[1]
-  #   on_board?(row, col + @index_counter) && @board[row][col].marker == @board[row][col + @index_counter].marker
-  # end
+  def horizontal?(coordinates)
+    row = coordinates[0]
+    col = coordinates[1]
+    on_board?(row, col + @index_counter) && @board[row][col].marker == @board[row][col + @index_counter].marker
+  end
 
-  # def vertical?(coordinates)
-  #   row = coordinates[0]
-  #   col = coordinates[1]
-  #   on_board?(row + @index_counter, col) && @board[row][col].marker == @board[row + @index_counter][col].marker
-  # end
+  def vertical?(coordinates)
+    row = coordinates[0]
+    col = coordinates[1]
+    on_board?(row + @index_counter, col) && @board[row][col].marker == @board[row + @index_counter][col].marker
+  end
 
-  # def forwardslash_diagonal?(coordinates)
-  #   row = coordinates[0]
-  #   col = coordinates[1]
-  #   on_board?(row - @index_counter, col + @index_counter) && @board[row][col].marker == @board[row - @index_counter][col + @index_counter].marker
-  # end
+  def forwardslash_diagonal?(coordinates)
+    row = coordinates[0]
+    col = coordinates[1]
+    on_board?(row - @index_counter, col + @index_counter) && @board[row][col].marker == @board[row - @index_counter][col + @index_counter].marker
+  end
 
-  # def backslash_diagonal?(coordinates)
-  #   row = coordinates[0]
-  #   col = coordinates[1]
-  #   on_board?(row + @index_counter, col + @index_counter) && @board[row][col].marker == @board[row + @index_counter][col + @index_counter].marker
-  # end
-
-  # def check_for_winner(player, coordinates)
-  #   if four_consecutive?( horizontal?(coordinates) ) || four_consecutive?( vertical?(coordinates) ) || four_consecutive?( backslash_diagonal?(coordinates) ) || four_consecutive?( forwardslash_diagonal?(coordinates) )
-  #     puts "#{player} Wins!"
-  #     return true
-  #   end
-  # end
+  def backslash_diagonal?(coordinates)
+    row = coordinates[0]
+    col = coordinates[1]
+    on_board?(row + @index_counter, col + @index_counter) && @board[row][col].marker == @board[row + @index_counter][col + @index_counter].marker
+  end
 
   def check_for_winner(player, coordinates)
-    if horizontal_winner?(coordinates) || vertical_winner?(coordinates) || backslash_diagonal_winner?(coordinates) || forwardslash_diagonal_winner?(coordinates)
+    if four_consecutive?( horizontal?(coordinates) ) || four_consecutive?( vertical?(coordinates) ) || four_consecutive?( backslash_diagonal?(coordinates) ) || four_consecutive?( forwardslash_diagonal?(coordinates) )
       puts "#{player} Wins!"
       return true
     end
   end
 
-  def horizontal_winner?(coordinates)
-    win_counter = 1
-    index_counter = 1
-    row = coordinates[0]
-    col = coordinates[1]
-    while win_counter < 4
-      if on_board?(row, col+index_counter) && @board[row][col].marker == @board[row][col+index_counter].marker
-        win_counter += 1
-        if index_counter < 0
-          index_counter -= 1
-        else
-          index_counter += 1
-        end
-      elsif index_counter < 0
-        break
-      else
-        index_counter = -1
-      end
-    end
-    return win_counter == 4
-  end
+  # def check_for_winner(player, coordinates)
+  #   if horizontal_winner?(coordinates) || vertical_winner?(coordinates) || backslash_diagonal_winner?(coordinates) || forwardslash_diagonal_winner?(coordinates)
+  #     puts "#{player} Wins!"
+  #     return true
+  #   end
+  # end
 
-  def vertical_winner?(coordinates)
-    win_counter = 1
-    index_counter = 1
-    row = coordinates[0]
-    col = coordinates[1]
-    while win_counter < 4
-      if on_board?(row+index_counter, col) && @board[row][col].marker == @board[row + index_counter][col].marker
-        win_counter += 1
-        if index_counter < 0
-          index_counter -= 1
-        else
-          index_counter += 1
-        end
-      elsif index_counter < 0
-        break
-      else
-        index_counter = -1
-      end
-    end
-    return win_counter == 4
-  end
+  # def horizontal_winner?(coordinates)
+  #   win_counter = 1
+  #   index_counter = 1
+  #   row = coordinates[0]
+  #   col = coordinates[1]
+  #   while win_counter < 4
+  #     if on_board?(row, col+index_counter) && @board[row][col].marker == @board[row][col+index_counter].marker
+  #       win_counter += 1
+  #       if index_counter < 0
+  #         index_counter -= 1
+  #       else
+  #         index_counter += 1
+  #       end
+  #     elsif index_counter < 0
+  #       break
+  #     else
+  #       index_counter = -1
+  #     end
+  #   end
+  #   return win_counter == 4
+  # end
 
-  def backslash_diagonal_winner?(coordinates)
-    win_counter = 1
-    index_counter = 1
-    row = coordinates[0]
-    col = coordinates[1]
-    while win_counter < 4
-      if on_board?(row+index_counter, col+index_counter) && @board[row][col].marker == @board[row+index_counter][col+index_counter].marker
-        win_counter += 1
-        if index_counter < 0
-          index_counter -= 1
-        else
-          index_counter += 1
-        end
-      elsif index_counter < 0
-        break
-      else
-        index_counter = -1
-      end
-    end
-    return win_counter == 4
-  end
+  # def vertical_winner?(coordinates)
+  #   win_counter = 1
+  #   index_counter = 1
+  #   row = coordinates[0]
+  #   col = coordinates[1]
+  #   while win_counter < 4
+  #     if on_board?(row+index_counter, col) && @board[row][col].marker == @board[row + index_counter][col].marker
+  #       win_counter += 1
+  #       if index_counter < 0
+  #         index_counter -= 1
+  #       else
+  #         index_counter += 1
+  #       end
+  #     elsif index_counter < 0
+  #       break
+  #     else
+  #       index_counter = -1
+  #     end
+  #   end
+  #   return win_counter == 4
+  # end
 
-  def forwardslash_diagonal_winner?(coordinates)
-    win_counter = 1
-    index_counter = 1
-    row = coordinates[0]
-    col = coordinates[1]
-    while win_counter < 4
-      if on_board?(row-index_counter, col+index_counter) && @board[row][col].marker == @board[row-index_counter][col+index_counter].marker
-        win_counter += 1
-        if index_counter < 0
-          index_counter -= 1
-        else
-          index_counter += 1
-        end
-      elsif index_counter < 0
-        break
-      else
-        index_counter = -1
-      end
-    end
-    return win_counter == 4
-  end
+  # def backslash_diagonal_winner?(coordinates)
+  #   win_counter = 1
+  #   index_counter = 1
+  #   row = coordinates[0]
+  #   col = coordinates[1]
+  #   while win_counter < 4
+  #     if on_board?(row+index_counter, col+index_counter) && @board[row][col].marker == @board[row+index_counter][col+index_counter].marker
+  #       win_counter += 1
+  #       if index_counter < 0
+  #         index_counter -= 1
+  #       else
+  #         index_counter += 1
+  #       end
+  #     elsif index_counter < 0
+  #       break
+  #     else
+  #       index_counter = -1
+  #     end
+  #   end
+  #   return win_counter == 4
+  # end
+
+  # def forwardslash_diagonal_winner?(coordinates)
+  #   win_counter = 1
+  #   index_counter = 1
+  #   row = coordinates[0]
+  #   col = coordinates[1]
+  #   while win_counter < 4
+  #     if on_board?(row-index_counter, col+index_counter) && @board[row][col].marker == @board[row-index_counter][col+index_counter].marker
+  #       win_counter += 1
+  #       if index_counter < 0
+  #         index_counter -= 1
+  #       else
+  #         index_counter += 1
+  #       end
+  #     elsif index_counter < 0
+  #       break
+  #     else
+  #       index_counter = -1
+  #     end
+  #   end
+  #   return win_counter == 4
+  # end
 
 end
